beffe44add078ff9dbf58877445aa46f0b663537
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-03-15 02:29:07 +0900

    Create 3d.js

---
+++ [개인과제-2]/3d.js
@@ -0,0 +1,15 @@
+let img;
+function setup()
+{
+  createCanvas(400, 400, WEBGL);
+  img = loadImage("shim.jpeg");
+}
+
+function draw()
+{
+  background(200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  texture(img);
+  box(200);
+}
