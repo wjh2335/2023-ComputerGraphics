e6b9f2cbf9aa5d51a36fe0e597e48a481fe174fc
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-03-21 18:38:00 +0900

    Update 지형생성.js

--- [팀과제-2]/지형생성.js
+++ [팀과제-2]/지형생성.js
@@ -1,10 +1,3 @@
-// Daniel Shiffman
-// http://codingtra.in
-// https://youtu.be/IKB1hWWedMk
-// https://thecodingtrain.com/CodingChallenges/011-perlinnoiseterrain.html
-
-// Edited by SacrificeProductions
-
 let cols, rows;
 let scl = 20;
 let w = 1400;
@@ -14,42 +7,47 @@ let flying = 0;
 
 let terrain = [];
 
-function setup() {
-  
+function setup()
+{
   createCanvas(600, 600, WEBGL);
   cols = w / scl;
   rows = h / scl;
   noStroke();
 
-  for (var x = 0; x < cols; x++) {
+  for (var x = 0; x < cols; x++)
+  {
     terrain[x] = [];
-    for (var y = 0; y < rows; y++) {
+    for (var y = 0; y < rows; y++)
+    {
       terrain[x][y] = 0; //specify a default value for now
     }
   }
 }
-function draw() {
-  
+function draw()
+{
   flying -= 0.1;
   let yoff = flying;
-  for (var y = 0; y < rows; y++) {
+  for (var y = 0; y < rows; y++)
+  {
     var xoff = 0;
-    for (var x = 0; x < cols; x++) {
+    for (var x = 0; x < cols; x++)
+    {
       terrain[x][y] = map(noise(xoff, yoff), 0, 1, -50, 50);
       xoff += 0.2;
     }
     yoff += 0.2;
   }
 
-
   background(255);
   translate(0, 50);
   rotateX(PI / 3);
   fill(200, 200, 200, 150);
   translate(-w / 2, -h / 2);
-  for (let y = 0; y < rows - 1; y++) {
+  for (let y = 0; y < rows - 1; y++)
+  {
     beginShape(TRIANGLE_STRIP);
-    for (let x = 0; x < cols; x++) {
+    for (let x = 0; x < cols; x++)
+    {
       let v = terrain[x][y];
       fill(v-32, v-64, v+128)
       vertex(x * scl, y * scl, terrain[x][y]);
