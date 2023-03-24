7ee0a1e6b664ae610b1954fda2f77dac496e543f
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-03-24 17:38:16 +0900

    Update 지형생성.js

--- [팀과제-2]/지형생성.js
+++ [팀과제-2]/지형생성.js
@@ -1,63 +1,80 @@
 let cols, rows;
 let scl = 20;
-let w = 1400;
-let h = 1000;
-
-let flying = 0;
-
+let w = 1400, h = 1000;
+let flyingX = 0, flyingY = 0;
 let terrain = [];
+let img;
+
+function preload()
+{
+  img = loadImage("https://raw.githubusercontent.com/wjh2335/2023-ComputerGraphics/main/img/6.png");
+}
 
 function setup()
 {
   createCanvas(600, 600, WEBGL);
   cols = w / scl;
   rows = h / scl;
-  noStroke();
 
-  for (var x = 0; x < cols; x++)
+  for (let x = 0; x < cols; x++)
   {
     terrain[x] = [];
-    for (var y = 0; y < rows; y++)
+    for (let y = 0; y < rows; y++)
     {
-      terrain[x][y] = 0; //specify a default value for now
+      terrain[x][y] = 0;
     }
   }
 }
+
 function draw()
 {
-  flying -= 0.1;
-  let yoff = flying;
-  for (var y = 0; y < rows; y++)
+  push();
+  noStroke();
+  translate(0, -200, -100);
+  texture(img);
+  plane(1000, 1000);
+  pop();
+  
+  if(keyIsDown(LEFT_ARROW)) { flyingX -= 0.1; }
+  if(keyIsDown(RIGHT_ARROW)) { flyingX += 0.1; }
+  if(keyIsDown(UP_ARROW)) { flyingY -= 0.1; }
+  if(keyIsDown(DOWN_ARROW)) { flyingY += 0.1; }
+  
+  var xoff = flyingX, yoff = flyingY;
+  
+  for (let y = 0; y < rows; y++)
   {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++)
+    xoff = flyingX;
+    for (let x = 0; x < cols; x++)
     {
       terrain[x][y] = map(noise(xoff, yoff), 0, 1, -50, 50);
       xoff += 0.2;
     }
     yoff += 0.2;
   }
-
-  background(255);
-  translate(0, 50);
+  
+  push();
+  translate(0, 0);
   rotateX(PI / 3);
-  fill(200, 200, 200, 150);
+  stroke(161, 148, 116);
+  fill(211, 198, 166);
   translate(-w / 2, -h / 2);
   for (let y = 0; y < rows - 1; y++)
   {
     beginShape(TRIANGLE_STRIP);
     for (let x = 0; x < cols; x++)
     {
-      let v = terrain[x][y];
-      fill(v-32, v-64, v+128)
       vertex(x * scl, y * scl, terrain[x][y]);
       vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
     }
     endShape();
   }
-  push(); translate(w / 2, h / 2);
-  translate(mouseX - width / 2, (mouseY - height / 2) * 6);
-  rotate(PI / 5);
-  normalMaterial();
-  sphere(100); pop();
+  pop();
+  
+  push();
+  noStroke();
+  translate(0, -0, 300);
+  fill(150);
+  torus(150, 50);
+  pop();
 }
