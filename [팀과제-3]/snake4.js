eccef312ade55b90dab83dbf0a688a91b7bc62fe
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-04-02 19:59:30 +0900

    Update snake4.js

--- [팀과제-3]/snake4.js
+++ [팀과제-3]/snake4.js
@@ -8,7 +8,7 @@ playfield = 600;
 
 function preload()
 {
-  img = loadImage("https://raw.githubusercontent.com/wjh2335/2023-ComputerGraphics/main/img/16.jpg");
+  img = loadImage("https://raw.githubusercontent.com/wjh2335/2023-ComputerGraphics/main/img/17.jpg");
 }
 
 function setup()
@@ -165,7 +165,61 @@ function Snake()
     fill(this.headColor);
     for (let i = 0; i < this.tail.length; i++)
     {
-        rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        //rect(this.tail[i].x, this.tail[i].y, scl, scl);
+      push();
+    
+    if(s.xspeed == 0 && s.yspeed == 1) { translate(this.tail[i].x - 10, this.tail[i].y - 10);  }
+    else if(s.xspeed == 0 && s.yspeed == -1) { translate(this.tail[i].x - 10, this.tail[i].y - 10);  }
+    else if(s.xspeed == 1 && s.yspeed == 0) { translate(this.tail[i].x - 10, this.tail[i].y - 10);  }
+    else if(s.xspeed == -1 && s.yspeed == 0) { translate(this.tail[i].x - 10, this.tail[i].y - 10);  }
+    scale(0.1);
+    fill(this.headColor);
+    strokeWeight(3);
+    
+    //귀
+    push(); //왼쪽 귀
+    translate(150, 115);
+    rotate(radians(-10));
+    ellipse(0, 0, 50, 100);
+    pop();
+    push(); //오른쪽 귀
+    translate(250, 115);
+    rotate(radians(10));
+    ellipse(0, 0, 50, 100);
+    pop();
+  
+    //귀 내부
+    fill(255, 180, 180);
+    push(); //왼쪽 귀 내부
+    translate(150, 112);
+    rotate(radians(-10));
+    ellipse(0, 0, 30, 65);
+    pop();
+    push(); //오른쪽 귀 내부
+    translate(250, 112);
+    rotate(radians(10));
+    ellipse(0, 0, 30, 65);
+    pop();
+  
+    //얼굴
+    fill(this.headColor);
+    ellipse(200, 200, 180, 170);
+  
+    //눈
+    fill(0);
+    ellipse(160, 195, 10, 10); //왼쪽 눈
+    ellipse(240, 195, 10, 10); //오른쪽 눈
+
+    //코
+    ellipse(200, 210, 15, 15);
+  
+    //입
+    fill(this.headColor);
+    line(200, 220, 200, 225); //인중
+    arc(190, 225, 20, 20, 0, PI); //왼쪽 입
+    arc(210, 225, 20, 20, 0, PI); //오른쪽 입
+  
+    pop();
     }
     //rect(this.x, this.y, scl, scl);
     
