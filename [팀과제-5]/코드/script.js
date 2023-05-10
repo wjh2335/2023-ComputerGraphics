a90b1519ca1e20e3d8097d03858f25d4df9b5ea7
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2023-05-10 13:04:49 +0900

    Update script.js

--- [팀과제-5]/코드/script.js
+++ [팀과제-5]/코드/script.js
@@ -12,8 +12,7 @@ Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
-function startVideo()
-{
+function startVideo() {
     navigator.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
@@ -23,16 +22,13 @@ function startVideo()
 
 startVideo();
 
-function capture()
-{
+function capture() {
     const context = canvas.getContext('2d');
     context.drawImage(video, 0, 0, canvas.width, canvas.height); // 캡처한 이미지 그리기
 }
 
-function capturePicture()
-{
-    if (!isCanvasCreated)
-    {
+function capturePicture() {
+    if (!isCanvasCreated) {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         isCanvasCreated = true;
@@ -46,6 +42,62 @@ function capturePicture()
 
     const firstRow = table.querySelector('tr:first-child');
     table.replaceChild(tr, firstRow); // 테이블 첫 번째 행 앞에 새로운 행 추가
+}
+
+async function start() {
+    const labeledFaceDescriptors = await loadLabeledImages();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+
+    attendBtn.addEventListener('click', async () => {
+        // 얼굴 인식
+        const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
+
+        // 인식된 얼굴과 일치하는 라벨 찾기
+        const results = detections.map(d => faceMatcher.findBestMatch(d.descriptor));
+        const labels = results.map(r => r.label);
+
+        // 일치하는 라벨이 있는 경우
+        if (labels.length > 0) {
+            for (let i = 0; i < labels.length; i++) {
+                const label = labels[i];
+                if (label == "unknown") continue;
+
+                const tr = table.querySelector(`tr[data-name="${label}"]`);
+                const td = tr.querySelector('td:last-child');
+                td.textContent = 'O';
+            }
+        }
+    });
+}
+
+function loadLabeledImages() {
+    const labels = ['Woo', 'Su', 'Captain America']
+    return Promise.all(
+        labels.map(async label => {
+            const descriptions = []
+            for (let i = 1; i <= 2; i++) {
+                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/wjh2335/test5/main/labeled_images/${label}/${i}.jpg`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+            }
+
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
+}
+
+function createExcel() {
+    // 엑셀 파일 생성
+    const workbook = XLSX.utils.book_new();
+
+    // 시트 생성
+    const sheet = XLSX.utils.table_to_sheet(table);
+
+    // 시트를 워크북에 추가
+    XLSX.utils.book_append_sheet(workbook, sheet, 'Attendance');
+
+    // 엑셀 파일 다운로드
+    XLSX.writeFile(workbook, 'attendance.xlsx');
 
     const tr1 = table.querySelector(`tr[data-name="Woo"]`);
     const td1 = tr1.querySelector('td:last-child');
@@ -62,85 +114,6 @@ function capturePicture()
     const tr4 = table.querySelector(`tr[data-name="Captain America"]`);
     const td4 = tr4.querySelector('td:last-child');
     td4.textContent = '';
-}
-
-async function start()
-{
-    const labeledFaceDescriptors = await loadLabeledImages();
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
-
-    attendBtn.addEventListener('click', async () => {
-        // 얼굴 인식
-        const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
-
-        // 인식된 얼굴과 일치하는 라벨 찾기
-        const results = detections.map(d => faceMatcher.findBestMatch(d.descriptor));
-        const labels = results.map(r => r.label);
-
-        const tr1 = table.querySelector(`tr[data-name="Woo"]`);
-        const td1 = tr1.querySelector('td:last-child');
-        td1.textContent = 'X';
-        
-        const tr2 = table.querySelector(`tr[data-name="Su"]`);
-        const td2 = tr2.querySelector('td:last-child');
-        td2.textContent = 'X';
-        
-        const tr3 = table.querySelector(`tr[data-name="Sin"]`);
-        const td3 = tr3.querySelector('td:last-child');
-        td3.textContent = 'X';
-        
-        const tr4 = table.querySelector(`tr[data-name="Captain America"]`);
-        const td4 = tr4.querySelector('td:last-child');
-        td4.textContent = 'X';
-            
-        
-        // 일치하는 라벨이 있는 경우
-        if (labels.length > 0)
-        {
-            for (let i = 0; i < labels.length; i++)
-            {
-                const label = labels[i];
-                if (label == "unknown") continue;
-
-                const tr = table.querySelector(`tr[data-name="${label}"]`);
-                const td = tr.querySelector('td:last-child');
-                td.textContent = 'O';
-            }
-        }
-    });
-}
-
-function loadLabeledImages()
-{
-    const labels = ['Woo', 'Captain America']
-    return Promise.all(
-        labels.map(async label => {
-            const descriptions = []
-            for (let i = 1; i <= 2; i++)
-            {
-                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/wjh2335/test4/main/labeled_images/${label}/${i}.jpg`)
-                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                descriptions.push(detections.descriptor)
-            }
-
-            return new faceapi.LabeledFaceDescriptors(label, descriptions)
-        })
-    )
-}
-
-function createExcel()
-{
-    // 엑셀 파일 생성
-    const workbook = XLSX.utils.book_new();
-
-    // 시트 생성
-    const sheet = XLSX.utils.table_to_sheet(table);
-
-    // 시트를 워크북에 추가
-    XLSX.utils.book_append_sheet(workbook, sheet, 'Attendance');
-
-    // 엑셀 파일 다운로드
-    XLSX.writeFile(workbook, 'attendance.xlsx');
 }
 
 captureBtn.addEventListener('click', capturePicture);
